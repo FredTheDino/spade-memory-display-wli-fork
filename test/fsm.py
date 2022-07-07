e32f1883c9f5e51b8b5c1f148e31d2aafdafc88e
TheZoq2 <frans.skarman@protonmail.com>
2022-07-07 12:11:09 +0200

    Reduce runtime of fsm test to only 3 lines for performance

--- test/fsm.py
+++ test/fsm.py
@@ -10,9 +10,9 @@ async def spi_state_transition(s: SpadeExt, clk, new):
     s.i.spi_falling_edge = "true"
     await FallingEdge(clk)
     s.i.spi_falling_edge = "false"
-    # s.o().assert_eq(new)
+    s.o().assert_eq(new)
     await FallingEdge(clk)
-    # s.o().assert_eq(new)
+    s.o().assert_eq(new)
 
 @cocotb.test()
 async def fsm(dut):
@@ -28,22 +28,22 @@ async def fsm(dut):
     await FallingEdge(clk)
     s.i.rst = "false"
 
-    for line in range(0, 240):
+    for line in range(0, 3):
         # Initial state is CsLow
-        # s.o().assert_eq(f"proj::main::State::CsLow({line}, 0)")
+        s.o().assert_eq(f"proj::main::State::CsLow({line}, 0)")
 
         # Wait low time (us1)
         [await FallingEdge(clk) for _ in range(0,5)]
         # Now high
-        # s.o().assert_eq(f"proj::main::State::CsHigh({line}, 0)")
+        s.o().assert_eq(f"proj::main::State::CsHigh({line}, 0)")
 
         # Wait low time (us1)
         [await FallingEdge(clk) for _ in range(0,5)]
-        # s.o().assert_eq(f"proj::main::State::CsHigh({line}, 4)")
+        s.o().assert_eq(f"proj::main::State::CsHigh({line}, 4)")
 
         await FallingEdge(clk)
         # Hold until spi falling edge
-        # s.o().assert_eq(f"proj::main::State::CsHigh({line}, 4)")
+        s.o().assert_eq(f"proj::main::State::CsHigh({line}, 4)")
 
         await spi_state_transition(s, clk, f"proj::main::State::Mode({line})")
         await spi_state_transition(s, clk, f"proj::main::State::FrameInv({line})")
@@ -57,8 +57,8 @@ async def fsm(dut):
 
         s.i.spi_falling_edge = "true"
         for x in range(0, 400):
-            # await spi_state_transition(s, clk, f"proj::main::State::Data({line}, {x})")
-            await FallingEdge(clk)
+            await spi_state_transition(s, clk, f"proj::main::State::Data({line}, {x})")
+            # await FallingEdge(clk)
 
         for x in range(0, 16):
             await spi_state_transition(s, clk, f"proj::main::State::EndDummy({line}, {x})")
@@ -66,12 +66,12 @@ async def fsm(dut):
         s.i.spi_falling_edge = "true"
         await FallingEdge(clk)
         s.i.spi_falling_edge = "false"
-        # s.o().assert_eq(f"proj::main::State::EndCsh({line}, 0)")
+        s.o().assert_eq(f"proj::main::State::EndCsh({line}, 0)")
         [await FallingEdge(clk) for _ in range(0, 5)]
 
-        print(f"Done line {line} in {cocotb.utils.get_sim_time()}")
+        # print(f"Done line {line} in {cocotb.utils.get_sim_time()}")
 
     # Wraparound
-    s.o().assert_eq(f"proj::main::State::CsLow(0, 0)")
+    # s.o().assert_eq(f"proj::main::State::CsLow(0, 0)")
 
 
